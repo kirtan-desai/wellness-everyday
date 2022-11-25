c005f360ba66e5e9a46dc988ddcb2a80d1d14bcd
Kirtan Desai <kirtands44@gmail.com>
2022-11-24 20:48:35 -0500

    added logout functionality

--- app/CalendarPage.jsx
+++ app/CalendarPage.jsx
@@ -1,38 +1,44 @@
 "use client";
 
+import { signOut } from "next-auth/react";
 import { differenceInCalendarDays } from "date-fns";
 import Calendar from "react-calendar";
 import styles from "./page.module.css";
+import "./Calendar.css";
+
+function isSameDay(a, b) {
+    return differenceInCalendarDays(a, b) !== 0;
+}
+
+const disabledDates = [new Date(2022, 0, 1)];
+
+function tileDisabled({ date, view }) {
+    if (view === "month") {
+        return disabledDates.find((dDate) => isSameDay(dDate, date));
+    }
+}
+
+function tileClassName({ date, view }) {
+    if (tileDisabled({ date, view })) {
+        return "filtered";
+    }
+}
 
 export default function CalendarPage() {
-  function isSameDay(a, b) {
-    return differenceInCalendarDays(a, b) !== 0;
-  }
+    return (
+        <div>
+            <button onClick={() => signOut()} className={styles.start}>
+                Logout
+            </button>
 
-  const disabledDates = [new Date(2022, 0, 1)];
-
-  function tileDisabled({ date, view }) {
-    if (view === "month") {
-      return disabledDates.find((dDate) => isSameDay(dDate, date));
-    }
-  }
-
-  function tileClassName({ date, view }) {
-    if (tileDisabled({ date, view })) {
-      return "filtered";
-    }
-  }
-
-  return (
-    <div>
-      <div className={styles["calendar-container"]}>
-        <div className={styles.navbar}>test</div>
-        <Calendar
-          tileDisabled={tileDisabled}
-          tileContent={() => <div className={styles.mood}>emoji</div>}
-          tileClassName={tileClassName}
-        />
-      </div>
-    </div>
-  );
+            <div className={styles["calendar-container"]}>
+                <div className={styles.navbar}>test</div>
+                <Calendar
+                    tileDisabled={tileDisabled}
+                    tileContent={() => <div className={styles.mood}>emoji</div>}
+                    tileClassName={tileClassName}
+                />
+            </div>
+        </div>
+    );
 }
