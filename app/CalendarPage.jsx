9e1473fc72a492550ea929b9cb4c5b47e2cc4716
Tanvi Khosla <tanvi@vt.edu>
2022-11-25 18:19:00 -0500

    date formatting

--- app/CalendarPage.jsx
+++ app/CalendarPage.jsx
@@ -3,32 +3,43 @@
 import { differenceInCalendarDays } from "date-fns";
 import Calendar from "react-calendar";
 import styles from "./page.module.css";
+import React, { useState } from "react";
 import "./Calendar.css"; //overriding default react-calendar css
 
+function isSameDay(a, b) {
+  return differenceInCalendarDays(a, b) !== 0;
+}
+
+const disabledDates = [];
+
+function tileDisabled({ date, view }) {
+  if (view === "month") {
+    return disabledDates.find((dDate) => isSameDay(dDate, date));
+  }
+}
+
+function tileClassName({ date, view }) {
+  if (tileDisabled({ date, view })) {
+    return "filtered";
+  }
+}
+
 export default function CalendarPage() {
-  function isSameDay(a, b) {
-    return differenceInCalendarDays(a, b) !== 0;
+  const [value, setValue] = useState(new Date());
+
+  function onChange(value) {
+    setValue(value);
+    console.log(JSON.stringify(new Date(value)).slice(1, 11));
   }
-
-  const disabledDates = [new Date(2022, 0, 1)];
-
-  function tileDisabled({ date, view }) {
-    if (view === "month") {
-      return disabledDates.find((dDate) => isSameDay(dDate, date));
-    }
-  }
-
-  function tileClassName({ date, view }) {
-    if (tileDisabled({ date, view })) {
-      return "filtered";
-    }
-  }
-
   return (
     <div>
       <div className={styles["calendar-container"]}>
-        <div className={styles.navbar}>test</div>
+        <div className={styles.navbar}>
+          <p>Filter days by mood</p>
+        </div>
         <Calendar
+          onClickDay={onChange}
+          value={value}
           tileDisabled={tileDisabled}
           tileContent={() => <div className={styles.mood}>emoji</div>}
           tileClassName={tileClassName}
