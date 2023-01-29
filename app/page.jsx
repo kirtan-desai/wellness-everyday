ede8aa9ad02b83479bc28416afc2c139a416947a
Kirtan Desai <kirtands44@gmail.com>
2023-01-29 12:53:08 -0500

    update deps

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 import LandingPage from "./components/LandingPage";
 import LoggedIn from "./components/LoggedIn";
 import getData from "./utils/getData";
 
 export default async function Home() {
-    const session = await unstable_getServerSession();
+    const session = await getServerSession();
     return session ? (
         <LoggedIn entries={await getData(session.user.email)} user={session.user} />
     ) : (
